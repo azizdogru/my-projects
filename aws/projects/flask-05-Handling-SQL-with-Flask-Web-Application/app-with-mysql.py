e5e01179654b8bbd0eb9bca9eff45ca9213c1fe5
f4323-Aziz <109878113+f4323-Aziz@users.noreply.github.com>
2022-09-26 17:00:01 +0300

    Update app-with-mysql.py

--- aws/projects/flask-05-Handling-SQL-with-Flask-Web-Application/app-with-mysql.py
+++ aws/projects/flask-05-Handling-SQL-with-Flask-Web-Application/app-with-mysql.py
@@ -6,9 +6,9 @@ from flaskext.mysql import MySQL
 app = Flask(__name__)
 
 # Configure mysql database
-app.config['MYSQL_DATABASE_HOST'] = 'PLEASE WRITE YOUR DATABASE ENDPOINT HERE'
+app.config['MYSQL_DATABASE_HOST'] = 'clarusway.cl17m16gjspq.us-east-1.rds.amazonaws.com'
 app.config['MYSQL_DATABASE_USER'] = 'admin'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'Clarusway_1'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'Pl123456'
 app.config['MYSQL_DATABASE_DB'] = 'clarusway'
 app.config['MYSQL_DATABASE_PORT'] = 3306
 mysql = MySQL()
